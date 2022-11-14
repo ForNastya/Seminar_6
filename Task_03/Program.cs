fe21e933151f397ce25c852b1dca112925caf687
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-14 22:46:11 +0700

    Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,14 +1,16 @@
 ﻿// Написать программу масштабирования фигуры
-string Scaling(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double k)
+string text = "(1,2) (2,3) (4,5) (6,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+var data = text.Split(" ")
+                    .Select(item => item.Split(','))
+                    .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+                    .Select(point => (point.x * 10, point.y))
+                    .ToArray();
+for (int i = 0; i < data.Length; i++)
 {
-    x1 = x1 * k;
-    y1 = y1 * k;
-    x2 = x2 * k;
-    y2 = y2 * k;
-    x3 = x3 * k;
-    y3 = y3 * k;
-    x4 = x4 * k;
-    y4 = y4 * k;
-    return $"({x1}, {y1} ({x2}, {y2}) ({x3}, {y3}) ({x4}, {y4})";
+        Console.WriteLine(data[i].Item1*10);
 }
-Console.WriteLine(Scaling(0, 0, 2, 0, 2, 2, 0, 2, 0.5));
+    Console.WriteLine();
